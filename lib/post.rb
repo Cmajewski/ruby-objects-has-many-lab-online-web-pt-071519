fca8513f54960983e800fc7f0f798dc196c1b5f1
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-06 20:17:26 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,2 +1,17 @@
 class Post
-end
+  attr_reader :name, :author
+  @@all=[]
+
+  def initialize (name)
+    @name=name
+    save
+  end
+
+  def save
+    @@all<<self
+  end
+
+  def self.all?
+    @@all
+  end
+  end
